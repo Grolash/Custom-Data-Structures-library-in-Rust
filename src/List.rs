aa8f73997cddf16050173b1af46d13c586c421d9
Grolash <Simon.MICHEL@student.umons.ac.be>
2022-09-27 20:56:29 +0200

    some progress

--- src/List.rs
+++ src/List.rs
@@ -1,7 +1,36 @@
-struct List {
-    head:
+// Link types!
+struct Link<'a, D> {
+    data: D,
+    next: &'a mut Link<'a, D>,
 }
 
-struct Link {
+struct DoubleLink<'a, D> {
+    data: D,
+    next: &'a mut DoubleLink<'a, D>,
+    prev: &'a mut DoubleLink<'a, D>,
+
+}
+
+// Lists types!
+struct List<'a, D> {
+    head: &'a mut Link<'a, D>,
+    sorted: bool,
+}
+
+struct DoubleList<'a, D> {
+    head: &'a mut DoubleLink<'a, D>,
+    sorted: bool,
+}
+
+struct CustomList<'a, D> {
+    head: &'a mut DoubleLink<'a, D>,
+    tail: &'a mut DoubleLink<'a, D>,
+    sorted: bool,
+}
+
+
+// List methods!
+
+fn unsorted_search<D>(list: List<D>, data: D) -> bool{
 
 }
