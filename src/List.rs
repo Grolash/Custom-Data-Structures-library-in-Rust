2c2c429975e2f702e969132d925aa53de161aa9d
Grolash <Simon.MICHEL@student.umons.ac.be>
2022-10-04 18:16:01 +0200

    I don't want to do lists anymore

--- src/List.rs
+++ src/List.rs
@@ -1,10 +1,12 @@
+use std::borrow::Borrow;
+
 // Link types
 #[derive(Clone)]
 pub struct Link<D> {
     data: D,
     next: Option<Box<Link<D>>>,
 }
-
+#[derive(Clone)]
 pub struct DoubleLink<D> {
     data: D,
     next: Option<Box<DoubleLink<D>>>,
@@ -83,7 +85,7 @@ impl<D: std::cmp::PartialEq + std::cmp::PartialOrd + std::clone::Clone> List<D> 
 
     /// Inserts data into the list if not already present and returns true, else return false
     /// if data is already present because no operation has been done.
-    pub fn insert(mut self, new_data: D) -> bool {
+    pub fn insert(&mut self, new_data: D) -> bool {
         let (boolean, option) = self.search(new_data.clone());
         match boolean {
             true => { false } // No operations done because data is already in the list.
@@ -114,8 +116,56 @@ impl<D: std::cmp::PartialEq + std::cmp::PartialOrd + std::clone::Clone> List<D> 
         }
     }
 
+    pub fn delete(&mut self, data: &D) -> bool {
+        let (boolean, option) = self.search(data.clone());
+        match boolean {
+            false => { false } // No operation has been done because data was not in the list
+            true => {
+                match option {
+                    None => {
+                        self.head = None;
+                        true
+                    } // data is in the head of the list, list becomes empty
+                    Some(mut link) => {
+                        let next = &link.next.as_ref();
+                        if next.is_some(){
+                            let next_next = next.unwrap().next.as_ref();
+                            link.next = next_next.cloned();
+                            true
+                        }
+                        else { false }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
+impl<D> CustomList<D>  {
+
+    pub fn new() -> Self {
+        CustomList{ head: None, tail: None, sorted: true }
+    }
+
+    fn has(&self, data: &D) -> (bool){
+        let (boolean, _) = Self::search(self.head.as_ref(), data);
+        boolean
+    }
+
+    fn search(current_link: Option<&Box<DoubleLink<D>>>, data: &D) -> (bool, Option<&Box<DoubleLink<D>>>){
+        match current_link {
+            None => { (false, current_link) }
+            Some(unwrapped) => {
+                if data == unwrapped.data.borrow() { true }
+                else if data > unwrapped.data.borrow() {
+                    let next_link = unwrapped.next.as_ref();
+                    Self::search(next_link, data)
+                }
+            }
+        }
+    }
+}
 
 
 
